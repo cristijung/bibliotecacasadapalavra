518ff77858a621cef2e8f471fabe52df3e650240
CristinaJung <cristijung@gmail.com>
2025-08-07 16:16:44 -0300

    feito

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -7,13 +7,11 @@ export default function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.content}>
-        {/* Título principal da seção Hero */}
         <h2 className={styles.headline}>Sua Casa para Histórias e Conhecimento</h2>
-        {/* Subtítulo ou tagline */}
         <p className={styles.tagline}>
           Descubra mundos, aprenda algo novo e conecte-se com a magia da leitura.
         </p>
-        {/* Chamada para ação */}
+        {/*CTA */}
         <Link href="/catalogo" className={styles.ctaButton}>
           Explore Nosso Catálogo
         </Link>
