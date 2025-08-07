58a53f1b74548996deb492d694d47921a65c4960
CristinaJung <cristijung@gmail.com>
2025-08-07 17:15:26 -0300

    footer extraído

--- app/page.tsx
+++ app/page.tsx
@@ -106,19 +106,7 @@ export default async function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <div className={styles.container}>
-          <p>
-            &copy; 2025 Biblioteca Casa da Palavra. Todos os direitos
-            reservados.
-          </p>
-          <div className={styles.socialLinks}>
-            <a href="#" className={styles.socialIcon}>FB</a>
-            <a href="#" className={styles.socialIcon}>IG</a>
-            <a href="#" className={styles.socialIcon}>TW</a>
-          </div>
-        </div>
-      </footer>
+     
     </div>
   );
 }
